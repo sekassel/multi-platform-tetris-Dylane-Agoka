8d70c0ef1081889687950938d1a2db98fa23e298
Dylane-Agoka <dylaneagoka47@gmail.com>
2022-04-28 13:58:59 +0100

    Push tetris app on heroku

--- cross-platform-monorepo/projects/tetris/src/app/app.component.ts
+++ cross-platform-monorepo/projects/tetris/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {GameEngineLibService} from '../../../game-engine-lib/src/lib/game-engine-lib.service';
+import { ElectronService } from './core/services/electron.service';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +10,20 @@ import {GameEngineLibService} from '../../../game-engine-lib/src/lib/game-engine
 export class AppComponent {
   title = 'tetris';
 
-  constructor(private engineService: GameEngineLibService) {
-    // tslint:disable-next-line:no-console
-    console.info(engineService.testing);
+  // constructor(private engineService: GameEngineLibService) {
+  //   // tslint:disable-next-line:no-console
+  //   // console.info(engineService.testing);
+  // }
+
+  constructor(private electronService: ElectronService) {
+
+    if (electronService.isElectron) {
+      console.log("Run in electron");
+      console.log("Electron ipcRenderer", this.electronService.ipcRenderer);
+      console.log("NodeJS childProcess", this.electronService.childProcess);
+    } else {
+      console.log("Run in browser");
+    }
   }
+
 }
--- cross-platform-monorepo/projects/tetris/src/app/app.module.ts
+++ cross-platform-monorepo/projects/tetris/src/app/app.module.ts
@@ -4,6 +4,9 @@ import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {GameEngineLibModule} from '../../../game-engine-lib/src/lib/game-engine-lib.module';
+import { CoreModule } from './core/core.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,14 @@ import {GameEngineLibModule} from '../../../game-engine-lib/src/lib/game-engine-
   imports: [
     BrowserModule,
     AppRoutingModule,
-    GameEngineLibModule
+    GameEngineLibModule,
+    CoreModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent],
